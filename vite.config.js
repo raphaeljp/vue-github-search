282eda1752937303055a9e6f9fd460c76ac5fa27
Raphael Pereira <rjp.raphael@gmail.com>
2022-06-02 17:47:02 -0300

    refactor: use sass variables on breakpoints

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+const projectRoot = path.resolve(__dirname, './src')
+
 export default defineConfig({
   plugins: [
     vue()
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "${projectRoot}/assets/styles/reset";
+          @import "${projectRoot}/assets/styles/variables";
+        `
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': projectRoot,
     }
   }
 })
