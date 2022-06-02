037bb6e431ca068da02b30a467278826d42a0922
Raphael Pereira <rjp.raphael@gmail.com>
2022-06-02 16:17:28 -0300

    feat: added base setup for vuex

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+const state = {}
+const mutations = {}
+const actions = {}
+const getters = {}
+
+const store = createStore({
+    state,
+    getters,
+    actions,
+    mutations
+})
+
+export default store
